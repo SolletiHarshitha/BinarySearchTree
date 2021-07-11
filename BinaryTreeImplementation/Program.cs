0d646a045bfab7de8974a220c32f28cddd0b36a2
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-07-11 18:13:23 +0530

    [Harshitha] Create and add elements in Binary Search Tree

--- BinaryTreeImplementation/Program.cs
+++ BinaryTreeImplementation/Program.cs
@@ -7,6 +7,10 @@ namespace BinaryTreeImplementation
         static void Main(string[] args)
         {
             Console.WriteLine("-----------------Binary Search Tree--------------");
+            BinaryTree<int> binaryTree = new BinaryTree<int>(56);
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Display();
         }
     }
 }
